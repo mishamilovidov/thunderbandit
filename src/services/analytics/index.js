adb2056f13b403c5061c8ae5b129a82a11e44c45
Misha Milovidov <m.alexeyvich@gmail.com>
2019-06-01 05:09:46 +0000

    Update analytics

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,7 +1,7 @@
 export const meta = {
   Home: {
     title: 'THUNDER BANDIT',
-    description: '"4" out now via SoundCloud. Listen to the new EP released January 30, 2019. All media created and produced by Thunder Bandit.',
+    description: '"5" out now via SoundCloud. Listen to the new EP released May 31, 2019. All media created and produced by Thunder Bandit.',
     canonical: 'https://thunderbandit.com/',
     meta: {
       charset: 'utf-8',
