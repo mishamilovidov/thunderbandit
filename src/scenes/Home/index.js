9922eb96c9bd77ec02ff09081d21473695ea1806
Misha Milovidov <m.alexeyvich@gmail.com>
2019-07-31 17:12:26 -0700

    💄 Update home scene with new single

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactGA from 'react-ga';
 import DocumentMeta from 'react-document-meta';
 import { meta } from '../../services/analytics';
-import { soundCloudCrowning } from '../../services/soundcloud';
+import { soundCloudSimple } from '../../services/soundcloud';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { transitionOptions, slideUpTransitionOptions } from '../../services/transitions';
 import './styles.css';
@@ -17,14 +17,14 @@ const Home = () => {
         </ReactCSSTransitionGroup>
         <div className="text">
           <ReactCSSTransitionGroup {...transitionOptions}>
-            <div className="subtitle">Setlist out now</div>
+            <div className="subtitle">Single out now</div>
           </ReactCSSTransitionGroup>
           <ReactCSSTransitionGroup {...slideUpTransitionOptions}>
-            <div className="title">CROWNING</div>
+            <div className="title">Simple</div>
             <div className="callToAction">
               <ReactGA.OutboundLink
                 eventLabel="soundCloudCrowning"
-                to={soundCloudCrowning}>
+                to={soundCloudSimple}>
                 Listen on SoundCloud
               </ReactGA.OutboundLink>
             </div>
