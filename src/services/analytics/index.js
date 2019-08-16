c79446fb828b034778ae93fcb0e38a3ac7029071
Misha Milovidov <m.alexeyvich@gmail.com>
2019-08-15 18:17:23 -0700

    Update analytics

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,23 +1,23 @@
 export const meta = {
   Home: {
     title: 'THUNDER BANDIT',
-    description: '"Big Kahuna" single out now via SoundCloud. Listen to the new single released August 8, 2019. All media created and produced by Thunder Bandit.',
+    description: '"Ascended" single out now via SoundCloud. Listen to the new single released August 15, 2019. All media created and produced by Thunder Bandit.',
     canonical: 'https://thunderbandit.com/',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist'
+        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single'
       }
     }
   },
   Music: {
     title: 'Music | THUNDER BANDIT',
-    description: 'All of the music produced and created by Thunder Bandit from Sex on the Tarmac to Big Kahuna.',
+    description: 'All of the music produced and created by Thunder Bandit from Sex on the Tarmac to Ascended.',
     canonical: 'https://thunderbandit.com/music',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Big Kahuna single'
+        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single'
       }
     }
   }
