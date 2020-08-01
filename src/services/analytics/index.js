5b8a8387fb984cd34ecd49d84e200051ab9612e5
Misha Milovidov <mishamilovidov@icloud.com>
2020-07-31 19:46:54 -0700

    Update ga keywords

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,23 +1,23 @@
 export const meta = {
   Home: {
     title: 'THUNDER BANDIT',
-    description: '"7" EP out now via SoundCloud. Listen to the new setlist released March 16, 2020.',
+    description: '"Bodement" EP out now via SoundCloud. Listen to the new setlist released July 31, 2020.',
     canonical: 'https://thunderbandit.com/',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single'
+        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single,bodement'
       }
     }
   },
   Music: {
     title: 'Music | THUNDER BANDIT',
-    description: 'All of the music produced and created by Thunder Bandit from Sex on the Tarmac to Ascended.',
+    description: 'All of the music produced and created by Thunder Bandit from Ascended to Bodement.',
     canonical: 'https://thunderbandit.com/music',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single'
+        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single,bodement'
       }
     }
   }
