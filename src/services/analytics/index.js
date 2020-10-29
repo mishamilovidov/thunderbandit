6026449dfd56b5549083769304fbd86e6069cdc1
Misha Milovidov <mishamilovidov@icloud.com>
2020-10-28 22:39:15 -0700

    Update analytics

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,23 +1,23 @@
 export const meta = {
   Home: {
     title: 'THUNDER BANDIT',
-    description: '"Freestyle Side A & Z" EP out now via SoundCloud. Listen to the new album released August 23, 2020.',
+    description: '"The Story of Uriel" EP out now via SoundCloud. Listen to the new album released October 28, 2020.',
     canonical: 'https://thunderbandit.com/',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single,bodement'
+        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single,bodement,story of uriel'
       }
     }
   },
   Music: {
     title: 'Music | THUNDER BANDIT',
-    description: 'All of the music produced and created by Thunder Bandit from Ascended to Bodement.',
+    description: 'All of the music produced and created by Thunder Bandit from Ascended to The Story of Uriel.',
     canonical: 'https://thunderbandit.com/music',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single,bodement'
+        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single,bodement,story of uriel'
       }
     }
   }
