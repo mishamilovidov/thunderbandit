60c939f55d8c218fd16ce6bddc089dc7c1efef8c
Misha Milovidov <mishamilovidov@icloud.com>
2020-12-30 17:30:02 -0800

    Update GA

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,23 +1,23 @@
 export const meta = {
   Home: {
     title: 'THUNDER BANDIT',
-    description: '"The Story of Uriel" EP out now via SoundCloud. Listen to the new album released October 28, 2020.',
+    description: '"111" EP out now via SoundCloud. Listen to the new album released December 30, 2020.',
     canonical: 'https://thunderbandit.com/',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single,bodement,story of uriel'
+        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single,bodement,story of uriel,111'
       }
     }
   },
   Music: {
     title: 'Music | THUNDER BANDIT',
-    description: 'All of the music produced and created by Thunder Bandit from Ascended to The Story of Uriel.',
+    description: 'All of the music produced and created by Thunder Bandit from The Story of Uriel to 111.',
     canonical: 'https://thunderbandit.com/music',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single,bodement,story of uriel'
+        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single,bodement,story of uriel,111'
       }
     }
   }
