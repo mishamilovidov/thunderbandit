e23b7754f5418efd172805d495a92c5cecc6a3f4
Misha Milovidov <42074547+mishamilovidov@users.noreply.github.com>
2021-02-12 12:59:26 -0800

    chore: update analytics

--- src/services/analytics/index.js
+++ src/services/analytics/index.js
@@ -1,23 +1,23 @@
 export const meta = {
   Home: {
     title: 'THUNDER BANDIT',
-    description: '"73 37 89 44 34" Setlist out now via SoundCloud. Listen to the new album released January 28, 2021.',
+    description: '"The Legend of Metatron" EP out now via SoundCloud. Listen to the new album released February 12, 2021.',
     canonical: 'https://thunderbandit.com/',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single,bodement,story of uriel,111'
+        keywords: 'thunderbandit,@thunderbandit,thunder bandit,THUNDER BANDIT,Thunder Bandit,thunder,bandit,tarmac,sex,ep,released,set list,baskets,crowning setlist,ascended single,bodement,story of uriel,111,legend of metatron'
       }
     }
   },
   Music: {
     title: 'Music | THUNDER BANDIT',
-    description: 'All of the music produced and created by Thunder Bandit from "The Story of Uriel" to "73 37 89 44 34".',
+    description: 'All of the music produced and created by Thunder Bandit from "The Story of Uriel" to "The Legend of Metatron".',
     canonical: 'https://thunderbandit.com/music',
     meta: {
       charset: 'utf-8',
       name: {
-        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single,bodement,story of uriel,111,73 37 89 44 34'
+        keywords: 'thunderbandit music,@thunderbandit music,thunder bandit music,THUNDER BANDIT music,Thunder Bandit music,thunder,bandit,tarmac,sex,ep,released,Ascended single,bodement,story of uriel,111,73 37 89 44 34,legend of metatron'
       }
     }
   }
