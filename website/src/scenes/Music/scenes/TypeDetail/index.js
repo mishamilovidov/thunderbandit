2df79953a52c17c490fbe23c923bd3089648e7fa
Misha Milovidov <m.alexeyvich@gmail.com>
2021-07-25 19:14:02 -0600

    style: update title text on music type detail

--- website/src/scenes/Music/scenes/TypeDetail/index.js
+++ website/src/scenes/Music/scenes/TypeDetail/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -21,14 +22,57 @@ const TypeDetailWrapper = styled.div`
   }
 `;
 
+const Title = styled.div`
+  min-height: 100px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    min-height: 55px;
+  }
+`;
+
+const TitleText = styled.div`
+  font-size: 50px;
+  width: ${({ theme }) => theme.widths.content.xl};
+  margin: 0 auto;
+  margin-top: 0px;
+  font-weight: bold;
+  color: #ffffff;
+  text-transform: uppercase;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.xl}) {
+    width: ${({ theme }) => theme.widths.content.md};
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    font-size: 40px;
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    width: ${({ theme }) => theme.widths.content.sm};
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    font-size: 30px;
+  }
+`;
+
 const TypeDetail = props => {
   const {
     state: { theme }
   } = useContext(AppContext);
   const { match } = props;
-  console.log(match);
 
-  return <TypeDetailWrapper theme={theme}>HellasdfaoWorld</TypeDetailWrapper>;
+  return (
+    <TypeDetailWrapper theme={theme}>
+      <Title theme={theme}>
+        <TitleText theme={theme}>{match.params.type}</TitleText>
+      </Title>
+    </TypeDetailWrapper>
+  );
 };
 
 TypeDetail.propTypes = {
