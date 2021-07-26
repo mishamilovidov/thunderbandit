590be7c6506083539d48c367128c910bbf32306a
Misha Milovidov <m.alexeyvich@gmail.com>
2021-07-25 18:52:41 -0600

    refactor: rm css from music scene

--- website/src/scenes/Music/index.js
+++ website/src/scenes/Music/index.js
@@ -5,7 +5,6 @@ import styled from 'styled-components';
 import { AppContext } from '../../contexts';
 import { meta } from '../../services/analytics';
 import Section from './components/Section';
-import './styles.css';
 
 const MusicWrapper = styled.div`
   min-height: 100vh;
@@ -25,6 +24,51 @@ const MusicWrapper = styled.div`
   }
 `;
 
+const Title = styled.div`
+  background-color: ${({ theme }) => theme.colors.secondary};
+  min-height: 200px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    width: 100%;
+    text-align: center;
+    height: 150px;
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    min-height: 125px;
+  }
+`;
+
+const TitleText = styled.div`
+  font-size: 70px;
+  width: ${({ theme }) => theme.widths.content.xl};
+  margin: 0 auto;
+  margin-top: 0px;
+  font-weight: bold;
+  color: #ffffff;
+  text-transform: uppercase;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.xl}) {
+    width: ${({ theme }) => theme.widths.content.md};
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    font-size: 60px;
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    width: ${({ theme }) => theme.widths.content.sm};
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    font-size: 50px;
+  }
+`;
+
 const Music = () => {
   const {
     state: { theme }
@@ -33,9 +77,9 @@ const Music = () => {
   return (
     <DocumentMeta {...meta.Music}>
       <MusicWrapper className='Music' theme={theme}>
-        <div className='title'>
-          <div className='text'>Music</div>
-        </div>
+        <Title theme={theme}>
+          <TitleText theme={theme}>Music</TitleText>
+        </Title>
         <Section title='Albums' type='album' />
         <Section title='EPs' type='ep' />
         <Section title='Singles' type='single' />
