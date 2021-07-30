4c33b0829b85cf7fb6e5da47e9dcecda307bf288
Misha Milovidov <m.alexeyvich@gmail.com>
2021-07-29 20:47:21 -0600

    chore: item detail scene styling cont

--- website/src/scenes/Music/scenes/ItemDetail/index.js
+++ website/src/scenes/Music/scenes/ItemDetail/index.js
@@ -5,6 +5,7 @@ import { withRouter } from 'react-router-dom';
 import { AppContext } from '../../../../contexts';
 
 const ItemDetailWrapper = styled.div`
+  color: #ffffff;
   width: ${({ theme }) => theme.widths.content.xl};
   margin: 26px auto 0px auto;
 
@@ -19,6 +20,18 @@ const ItemDetailWrapper = styled.div`
   @media only screen and (max-width: ${({ theme }) => theme.breakpoints.md}) {
     width: ${({ theme }) => theme.widths.content.sm};
   }
+
+  @keyframes pulse {
+    0% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+    50% {
+      background-color: rgba(165, 165, 165, 0.3);
+    }
+    100% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+  }
 `;
 
 const ItemHeaderWrapper = styled.div`
@@ -26,20 +39,54 @@ const ItemHeaderWrapper = styled.div`
   justify-content: flex-start;
   align-items: center;
 
-  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.xs}) {
     display: block;
   }
 `;
 
 const ItemCoverArtWrapper = styled.div`
-  flex: 1;
+  flex: 4;
+`;
+
+const CoverArtWrapper = styled.div`
+  width: 100%;
+  padding-top: 100%; /* 1:1 Aspect Ratio */
+  position: relative;
+`;
+
+const CoverArt = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-image: url(${({ img }) => img});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-color: ${({ theme }) =>
+    theme.scenes.music.coverart.placeholderColor};
+  animation: ${({ loading }) =>
+    loading === 1 ? `pulse 1s infinite ease-in-out` : 'unset'};
+  border-radius: 6px;
 `;
 
 const ItemDetailsWrapper = styled.div`
-  flex: 2;
+  flex: 10;
+  margin-left: 40px;
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    margin-left: 24px;
+  }
+
+  @media only screen and (max-width: ${({ theme }) => theme.breakpoints.xs}) {
+    margin: 40px 0px 0px 0px;
+    text-align: center;
+  }
 `;
 
-const ItemTracksWrapper = styled.div``;
+const ItemTracksWrapper = styled.div`
+  margin-top: 40px;
+`;
 
 const ItemDetail = props => {
   const {
@@ -63,8 +110,14 @@ const ItemDetail = props => {
   return (
     <ItemDetailWrapper theme={theme}>
       <ItemHeaderWrapper theme={theme}>
-        <ItemCoverArtWrapper>ItemCoverArtWrapper</ItemCoverArtWrapper>
-        <ItemDetailsWrapper>ItemDetailsWrapper</ItemDetailsWrapper>
+        <ItemCoverArtWrapper>
+          <CoverArtWrapper>
+            <CoverArt theme={theme} img={null} loading={Number(true)} />
+          </CoverArtWrapper>
+        </ItemCoverArtWrapper>
+        <ItemDetailsWrapper theme={theme}>
+          ItemDetailsWrapper
+        </ItemDetailsWrapper>
       </ItemHeaderWrapper>
       <ItemTracksWrapper>ItemTracksWrapper</ItemTracksWrapper>
     </ItemDetailWrapper>
